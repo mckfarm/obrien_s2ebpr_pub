30e11290d0823be5f72f3a220ec2c04736d33007
Mckenna Farmer <mckennafarmer2023@u.northwestern.edu>
2024-01-29 14:08:25 -0600

    nitrospira corr numbers for table

--- scripts/nitrospira_corr.R
+++ scripts/nitrospira_corr.R
@@ -19,23 +19,35 @@ rel_genus %>%
   geom_point(size = 2.5, fill = "white", alpha = 0.8) + 
   scale_shape_manual(values = c(3, 25, 24, 22), name = "Genus") + 
   scale_color_manual(values = met.brewer("Johnson", 4), name = "Genus") + 
-  theme_black_box + theme(legend.text = element_text(face = "italic")) + 
+  theme_black_box + theme(legend.text = element_text(face = "italic"), legend.position = "left") + 
   x_axis_date +
   labs(x = "Date", y = "Relative abundance [%]")
 ggsave("results/nitrospira_cor.png", width = 10, height = 3.5, units = "in", dpi = 300)
 
 
-df_cor <- rel_genus %>% 
-  ungroup() %>%
-  filter(Genus %in% taxa_filt$Genus) %>%
-  filter(date <= ymd("2023-07-31")) %>%
-  filter(reactor == "SBR3") %>%
-  pivot_wider(names_from = Genus, values_from = sum) %>%
-  select(-c(date, reactor, sample))
+reactor_list <- c("SBR1", "SBR2", "SBR3")
 
-test_pear <- df_cor %>%
-  cor_test(vars = Ca_Competibacter, method = "pearson") 
+pearson_list <- list()
+for(i in reactor_list){
+  df_cor <- rel_genus %>% 
+    ungroup() %>%
+    select(-genus_format) %>%
+    filter(reactor == paste(i)) %>%
+    pivot_wider(names_from = Genus, values_from = sum) %>%
+    select(-c(date, reactor, sample)) %>%
+    select_if(~median(.) > 0.05)
+  
+  test_pear <- df_cor %>%
+    cor_test(vars = Ca_Competibacter, method = "pearson") 
+  
+  test_pear_filt <- test_pear %>%
+    mutate(reactor = paste(i))
+  
+  pearson_list[[i]] <- test_pear_filt
+}
 
-test_pear_filt <- test_pear %>%
-  filter(p <= 0.05) 
+pearson_df <- do.call(rbind, pearson_list)
+
+pearson_df %>%
+  filter(var2 %in% c("midas_g_179", "Hassallia", "Nitrospira"))
 
